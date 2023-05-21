6dbac70b0c847fe3c4a75f0c7dd94c8859f089d7
PatrickJose00 <patrick_jose_00@hotmail.com>
2023-05-22 01:58:08 +0200

    trying to fix cors

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,4 +2,14 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  devServer: {
+    headers: { "Access-Control-Allow-Origin": "*" },
+    proxy: {
+      '/stocks': {
+        target: 'http://localhost:8000',
+        pathRewrite: { '^/stocks': '' },
+        changeOrigin: true
+      },
+    }
+}
 })
