1098f62d733f0a377eb7d7aa28ec162c6eaf5e4e
PatrickJose00 <patrick_jose_00@hotmail.com>
2023-05-22 01:26:59 +0200

    fix build problems

--- client/server.js
+++ client/server.js
@@ -1,17 +1,11 @@
 const express = require("express");
-const path = require("path");
-
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Set the static folder path
-const staticFolderPath = path.join(__dirname, "dist");
-app.use(express.static(staticFolderPath));
-
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(staticFolderPath, "index.html"));
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+  res.sendFile(__dirname + "/dist/index.html");
 });
+app.listen(port);
 
-app.listen(port, function() {
-  console.log("Server started on port", port);
-});
+console.log("Server started...");
