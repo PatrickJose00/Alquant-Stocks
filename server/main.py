c01aa76444139120eca007b82ff409f51c27dee8
PatrickJose00 <patrick_jose_00@hotmail.com>
2023-05-22 12:28:52 +0200

    fix cors issue

--- server/main.py
+++ server/main.py
@@ -1,25 +1,24 @@
 from fastapi import FastAPI
 from app.api.endpoints import stocks
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 import uvicorn
-import os
-
-app = FastAPI(debug=True)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['*'],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-    expose_headers=["*"])
 
 
-# Include the routers for your API
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*']
+    )
+]
+
+app = FastAPI(middleware=middleware)
+
 app.include_router(stocks.router)
-
-
 
 # Run the app with Uvicorn
 if __name__ == "__main__":
